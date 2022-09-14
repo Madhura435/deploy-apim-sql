a8efa2d7a7bcf24cc9f15f82b77352ca92fb8a03
Madhura Maddipatla <P7112792@NESSINBLR.COM>
2022-09-14 11:25:49 +0530

    spring-bbot

---
+++ src/main/java/com/azure/deployapimmysql/controller/EmployeeController.java
@@ -0,0 +1,43 @@
+package com.azure.deployapimmysql.controller;
+
+import com.azure.deployapimmysql.model.Employee;
+import com.azure.deployapimmysql.repository.EmployeeRepository;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/madhura/alone")
+@Slf4j
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @ApiOperation(value = "to return list of employees")
+    @GetMapping(value = "employees")
+    private List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @ApiOperation(value = "to return employee by id")
+    @GetMapping(value = "employee")
+    private Employee getByEmployeeId(@RequestParam("eid") String eid) {
+        return employeeRepository.findById(eid).get();
+    }
+
+    @ApiOperation(value = "save employee")
+    @PostMapping(value = "employee")
+    private ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+        Employee employee1 = employeeRepository.save(employee);
+        return new ResponseEntity<>(employee1, HttpStatus.OK);
+    }
+
+
+}
---
+++ src/test/java/com/azure/deployapimmysql/DeployApimMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azure.deployapimmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployApimMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
