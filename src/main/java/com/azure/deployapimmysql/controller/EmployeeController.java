20f7688bd4423c68471b8297232c3e2a5e53e7e5
madhura4533 <madhura.maddipatla453@gmail.com>
2022-09-14 14:41:16 +0530

    intial

--- src/main/java/com/azure/deployapimmysql/controller/EmployeeController.java
+++ src/main/java/com/azure/deployapimmysql/controller/EmployeeController.java
@@ -1,8 +1,8 @@
 package com.azure.deployapimmysql.controller;
 
-import com.azure.deployapimmysql.model.Employee;
-import com.azure.deployapimmysql.repository.EmployeeRepository;
-import io.swagger.annotations.ApiOperation;
+//import com.azure.deployapimmysql.model.Employee;
+//import com.azure.deployapimmysql.repository.EmployeeRepository;
+//import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,26 +17,31 @@ import java.util.List;
 @Slf4j
 public class EmployeeController {
 
-    @Autowired
-    private EmployeeRepository employeeRepository;
+//    @Autowired
+//    private EmployeeRepository employeeRepository;
 
-    @ApiOperation(value = "to return list of employees")
-    @GetMapping(value = "employees")
-    private List<Employee> getAllEmployees1() {
-        return employeeRepository.findAll();
-    }
+//    @ApiOperation(value = "to return list of employees")
+//    @GetMapping(value = "employees")
+//    private List<Employee> getAllEmployees1() {
+//        return employeeRepository.findAll();
+//    }
+//
+//    @ApiOperation(value = "to return employee by id")
+//    @GetMapping(value = "employee")
+//    private Employee getByEmployeeId(@RequestParam("eid") String eid) {
+//        return employeeRepository.findById(eid).get();
+//    }
+//
+//    @ApiOperation(value = "save employee")
+//    @PostMapping(value = "employee")
+//    private ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+//        Employee employee1 = employeeRepository.save(employee);
+//        return new ResponseEntity<>(employee1, HttpStatus.OK);
+//    }
 
-    @ApiOperation(value = "to return employee by id")
     @GetMapping(value = "employee")
-    private Employee getByEmployeeId(@RequestParam("eid") String eid) {
-        return employeeRepository.findById(eid).get();
-    }
-
-    @ApiOperation(value = "save employee")
-    @PostMapping(value = "employee")
-    private ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
-        Employee employee1 = employeeRepository.save(employee);
-        return new ResponseEntity<>(employee1, HttpStatus.OK);
+    private String getByEmployeeId(@RequestParam("eid") String eid) {
+        return "waste fellow madhura";
     }
 
 
