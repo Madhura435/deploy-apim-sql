060af24e56c8b6301982380424f34039e6fb73a7
madhura4533 <madhura.maddipatla453@gmail.com>
2022-09-16 16:31:05 +0530

    build

--- src/main/java/com/azure/deployapimmysql/controller/EmployeeController.java
+++ src/main/java/com/azure/deployapimmysql/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -21,19 +22,19 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     @ApiOperation(value = "to return list of employees")
-    @GetMapping(value = "employees")
+    @GetMapping(value = "employees",produces = MediaType.APPLICATION_JSON_VALUE)
     private List<Employee> getAllEmployees1() {
         return employeeRepository.findAll();
     }
 
     @ApiOperation(value = "to return employee by id")
-    @GetMapping(value = "employee")
+    @GetMapping(value = "employee",produces = MediaType.APPLICATION_JSON_VALUE)
     private Employee getByEmployeeId(@RequestParam("eid") String eid) {
         return employeeRepository.findById(eid).get();
     }
 
     @ApiOperation(value = "save employee")
-    @PostMapping(value = "employee")
+    @PostMapping(value = "employee",consumes =MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     private ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
         Employee employee1 = employeeRepository.save(employee);
         return new ResponseEntity<>(employee1, HttpStatus.OK);
