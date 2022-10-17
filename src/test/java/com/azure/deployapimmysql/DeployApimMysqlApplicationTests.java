af16d66b0adad306764ed10b97119664c831056a
madhura4533 <madhura.maddipatla453@gmail.com>
2022-10-17 22:13:35 +0530

    why failing ab

--- src/test/java/com/azure/deployapimmysql/DeployApimMysqlApplicationTests.java
+++ src/test/java/com/azure/deployapimmysql/DeployApimMysqlApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DeployApimMysqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
