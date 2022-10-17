97ded56ee2b36f5753f48dcd8c81a66059b8aa69
madhura4533 <madhura.maddipatla453@gmail.com>
2022-10-17 17:47:55 +0530

    why failing

--- src/main/java/com/azure/deployapimmysql/DeployApimMysqlApplication.java
+++ src/main/java/com/azure/deployapimmysql/DeployApimMysqlApplication.java
@@ -15,4 +15,15 @@ public class DeployApimMysqlApplication {
 	// In this project we are also doing azure App service with deployment. Here we are  in built Azure
 	// AD integration of app service.
 	// logout url for web app https://webapp-auth.azurewebsites.net/.auth/logout
+
+
+
+	// below is when your connection strings are placed in web app . you are trying to get that connection
+	// strings in your spring boot app
+	// String envStr = System.getenv("APPSETTING_TestSetting");
+	// my.secret.password=${SUPER_SECRET_ENV_VARIABLE}
+	//
+
+
+
 }
